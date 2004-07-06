3a8017c239aba1141f7bfd8e3940577bf34c005d
cvs_import <cvs_import>
2004-07-06 20:39:18 +0000

    beginning of branch i2p.www.i2pwww

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+define(PAGE_DIR,'pages/');
+define(PAGE_EXT,'.html');
+
+/* Filter out everything except alphanumerical characters and underscores */
+function form_filter($data) {
+    return preg_replace('/[^[:alnum:]_]/','',$data);
+}
+
+$page = '';
+if(isset($_REQUEST['page'])) {
+    $page = form_filter(urldecode($_REQUEST['page']));
+} else {
+    $page = 'home';
+}
+
+if(is_readable(PAGE_DIR.$page.PAGE_EXT)) {
+    include(PAGE_DIR.'header'.PAGE_EXT);
+    include(PAGE_DIR.$page.PAGE_EXT);
+    include(PAGE_DIR.'footer'.PAGE_EXT);
+} else {                                                                                            
+    header("HTTP/1.0 404 Not Found");
+    include(PAGE_DIR.'header'.PAGE_EXT);
+    print "<h1>Error: Page \"$page\" not found</h1>";
+    include(PAGE_DIR.'footer'.PAGE_EXT);
+}
+?>
