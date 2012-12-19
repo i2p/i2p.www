8f9e8fb178ded11160000c8f89f2a804114399ba
str4d <str4d@mail.i2p>
2012-12-19 05:28:01 +0000

    Added LazyView from http://flask.pocoo.org/docs/patterns/lazyloading/ to helpers

--- i2p2www/helpers.py
+++ i2p2www/helpers.py
@@ -1,4 +1,17 @@
 from math import ceil
+from werkzeug import import_string, cached_property
+
+class LazyView(object):
+    def __init__(self, import_name):
+        self.__module__, self.__name__ = import_name.rsplit('.', 1)
+        self.import_name = import_name
+
+    @cached_property
+    def view(self):
+        return import_string(self.import_name)
+
+    def __call__(self, *args, **kwargs):
+        return self.view(*args, **kwargs)
 
 class Pagination(object):
     def __init__(self, page, per_page, total_count):
