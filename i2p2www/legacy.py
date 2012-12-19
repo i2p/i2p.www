85c2ca464c147d84c41deff194e05d6b42bc08c4
str4d <str4d@mail.i2p>
2012-12-19 10:55:11 +0000

    Added a few legacy pages (and distinguished them from pages that are now functions)

--- i2p2www/legacy.py
+++ i2p2www/legacy.py
@@ -4,16 +4,24 @@ from flask import g, redirect, url_for
 ##############
 # Legacy paths
 
-LEGACY_MAP={
+LEGACY_FUNCTIONS_MAP={
     'download': 'downloads_list'
+}
+
+LEGACY_PAGES_MAP={
+    'bounties': 'volunteer/bounties',
+    'getinvolved': 'volunteer',
+    'faq': 'support/faq',
 }
 
 def legacy_show(f):
     lang = 'en'
     if hasattr(g, 'lang') and g.lang:
         lang = g.lang
-    if f in LEGACY_MAP:
-        return redirect(url_for(LEGACY_MAP[f], lang=lang))
+    if f in LEGACY_FUNCTIONS_MAP:
+        return redirect(url_for(LEGACY_FUNCTIONS_MAP[f], lang=lang))
+    elif f in LEGACY_PAGES_MAP:
+        return redirect(url_for('site_show', lang=lang, page=LEGACY_PAGES_MAP[f]))
     else:
         return redirect(url_for('site_show', lang=lang, page=f))
 
