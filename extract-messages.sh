e50d7e4003b8543bc6084bdb92fef50d4e4fb620
str4d <str4d@mail.i2p>
2013-01-18 11:18:13 +0000

    Tweaked message extraction script

--- extract-messages.sh
+++ extract-messages.sh
@@ -1,3 +1,3 @@
 source ./translation.vars
 
-TZ=UTC env/bin/pybabel extract --project=$PROJECT --version=$VERSION -F $BABELCFG $PROJDIR > $POTFILE
+TZ=UTC env/bin/pybabel extract --project=$PROJECT --version=$VERSION -F $BABELCFG -o $POTFILE $PROJDIR
