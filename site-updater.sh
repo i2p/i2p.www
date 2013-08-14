4e5b6790d12bb7acb9ca3dbe6b3c471cd0174370
str4d <str4d@mail.i2p>
2013-08-14 22:18:06 +0000

    Added site updater

---
+++ site-updater.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+TMP=tmp
+
+mtn update 2>&1 | tee $TMP
+
+cat $TMP | grep "^mtn: updating 'i2p2www/translations/" >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Translations updated, compiling messages"
+  ./compile-messages.sh
+fi
+
+cat $TMP | grep "^mtn: updating 'i2p2www/.*/.*\.py" >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Python files changed, restarting server"
+  supervisorctl restart mainsite
+fi
+
+rm $TMP
