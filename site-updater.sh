b403e74fd590202582e9f99e30b0cd74486c8f4e
str4d <str4d@mail.i2p>
2013-08-29 06:57:23 +0000

    Fixed detection of Python file changes

--- site-updater.sh
+++ site-updater.sh
@@ -9,7 +9,7 @@ if [ $? -eq 0 ]; then
   ./compile-messages.sh
 fi
 
-cat $TMP | grep "^mtn: updating 'i2p2www/.*/.*\.py" >/dev/null
+cat $TMP | grep "^mtn: updating 'i2p2www/.*\.py\|^mtn: updating 'i2p2www/.*/.*\.py" >/dev/null
 if [ $? -eq 0 ]; then
   echo "Python files changed, restarting server"
   touch /tmp/2fcd2f17-c293-4f77-b4c9-9b266ba70daa
