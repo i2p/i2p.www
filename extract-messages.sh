5a6cec476fe8f5e3168466e1d4f43558c0b4355c
str4d <str4d@mail.i2p>
2013-09-01 23:23:29 +0000

    Updated message extraction to only update changed .pots

--- extract-messages.sh
+++ extract-messages.sh
@@ -14,10 +14,17 @@ then
                                -o $POTDIR/$1.pot $PROJDIR
 else
     for domain in $(ls $BABELCFG); do
+        if [ -e $POTDIR/$domain.pot ]; then
+            mv $POTDIR/$domain.pot $POTDIR/$domain.pot.old
+        fi
         TZ=UTC ./pybabel extract --msgid-bugs-address="http://trac.i2p2.de" \
                                --project=$PROJECT \
                                --version=$VERSION \
                                -F $BABELCFG/$domain \
                                -o $POTDIR/$domain.pot $PROJDIR
+        diff -u $POTDIR/$domain.pot.old $POTDIR/$domain.pot | grep '^+' | grep -v '^+++' | grep -v '+"POT-Creation-Date' >/dev/null
+        if [ $? -eq 1 ]; then
+            mv $POTDIR/$domain.pot.old $POTDIR/$domain.pot
+        fi
     done
 fi
