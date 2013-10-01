5c6855b7c425d4c76c888237a7c01d93afbe8860
kytv <kytv@mail.i2p>
2013-10-01 11:51:00 +0000

    explicit mtn pull then mtn up

--- site-updater-i2hq.sh
+++ site-updater-i2hq.sh
@@ -2,6 +2,7 @@
 TMP=$(mktemp XXXXXXXXXX)
 trap 'rm -f $TMP;exit' 0 1 2 15
 
+mtn pull
 mtn update 2>&1 | tee $TMP
 
 if grep "^mtn: \(add\|dropp\|updat\)ing 'i2p2www/translations/" "$TMP" >/dev/null; then
