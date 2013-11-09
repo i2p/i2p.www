8c8a02874f485385547e6c4267073281b3d24552
str4d <str4d@mail.i2p>
2013-11-09 20:10:57 +0000

    Updated i2hq scripts to match regular ones

--- compile-messages-i2hq.sh
+++ compile-messages-i2hq.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 . ./etc/translation.vars
 
+export TZ=UTC
+
 if [ $# -ge 1 ]
 then
-    TZ=UTC pybabel compile -D $1 -d $TRANSDIR
+    pybabel compile -D $1 -d $TRANSDIR
 else
     for domain in $(ls $BABELCFG); do
-        TZ=UTC pybabel compile -D $domain -d $TRANSDIR
+        pybabel compile -D $domain -d $TRANSDIR
     done
 fi
--- site-updater-i2hq.sh
+++ site-updater-i2hq.sh
@@ -3,14 +3,15 @@ TMP=$(mktemp XXXXXXXXXX)
 trap 'rm -f $TMP;exit' 0 1 2 15
 
 mtn pull
-mtn update 2>&1 | tee $TMP
+mtn up 2>&1 | tee $TMP
 
-if grep "^mtn: \(add\|dropp\|updat\)ing 'i2p2www/translations/" "$TMP" >/dev/null; then
+
+if grep "^mtn: \(add\|patch\|dropp\|updat\)\(ed\|ing\) 'i2p2www/translations/" "$TMP" >/dev/null ; then
   echo "Translations updated, compiling messages"
   ./compile-messages-i2hq.sh
 fi
 
-if grep "^mtn: updating 'i2p2www/.*\.py\|^mtn: updating 'i2p2www/.*/.*\.py" "$TMP" >/dev/null; then
+if grep "^mtn: updating 'i2p2www/.*\.py\|^mtn: updating 'i2p2www/.*/.*\.py" "$TMP" >/dev/null ; then
   echo "Python files changed, restarting server"
   touch /tmp/2fcd2f17-c293-4f77-b4c9-9b266ba70daa
 fi
