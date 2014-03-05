17c265bdd7ae09e752e1182e2c8739ac942ef638
str4d <str4d@mail.i2p>
2014-03-05 02:43:53 +0000

    Strip out commented-out translations after update
    This is necessary because:
    - Transifex runs 'msgfmt -c' on any uploaded .po file
    - pybabel adds a spurious '#~ msgid ""' to the end of some files
    - 'msgfmt -c' ignores that this is commented out, and treats it
      as a syntax error (clashing with the top file definition)

--- update-existing-po.sh
+++ update-existing-po.sh
@@ -5,8 +5,14 @@ export TZ=UTC
 if [ $# -ge 1 ]
 then
     $PYBABEL update -D $1 -i $POTDIR/$1.pot -d $TRANSDIR
+    for file in $(ls i2p2www/translations/*/LC_MESSAGES/$1.po); do
+        sed -i '/^#~/,+2d' $file
+    done
 else
     for domain in $(ls $BABELCFG); do
         $PYBABEL update -D $domain -i $POTDIR/$domain.pot -d $TRANSDIR
+        for file in $(ls i2p2www/translations/*/LC_MESSAGES/$domain.po); do
+            sed -i '/^#~/,+2d' $file
+        done
     done
 fi
