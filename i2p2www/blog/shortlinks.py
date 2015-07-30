b5790851fae3d29154e2aac8673235de2fd2a61f
dev <dev@robertfoss.se>
2015-07-30 17:48:13 +0000

    Added new I2PControl calls to API level 1.

--- i2p2www/blog/shortlinks.py
+++ i2p2www/blog/shortlinks.py
@@ -8,6 +8,7 @@
 # '<shortlink>': '<slug>',
 
 BLOG_SHORTLINKS = {
+    'i2pcon': '2015/07/16/I2PCon',
     'toronto': '2015/07/16/I2PCon',
     '0.9.19': '2015/04/12/0.9.19-Release',
     '0.9.20': '2015/06/02/0.9.20-Release',
