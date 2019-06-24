761513e5cc0077bf7416325b32bd4795a6c2c257
hankhill19580 <hankhill19580@gmail.com>
2019-06-24 22:46:19 +0000

    update the logo and the css for the logo

--- i2p2www/templatevars.py
+++ i2p2www/templatevars.py
@@ -184,9 +184,9 @@ def utility_processor():
 
     # Shorthand for getting the logo for the current theme
     def get_logo_for_theme():
-        logo = 'styles/' + g.theme + '/images/i2plogo.png'
+        logo = 'styles/' + g.theme + '/images/i2plogo.svg'
         if not os.path.isfile(safe_join(STATIC_DIR, logo)):
-            logo = 'images/i2plogo.png'
+            logo = 'images/i2plogo.svg'
         return logo
 
     def get_current_version(string=None, ver=None):
--- runserver.py
+++ runserver.py
@@ -2,4 +2,4 @@
 from i2p2www import app
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
