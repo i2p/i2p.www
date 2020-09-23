add2fbb0c634d116c45ec35475594d7b6f2ac049
hankhill19580 <hankhill19580@gmail.com>
2020-09-23 23:52:39 +0000

    cd to source directory in docker updater script

--- site-updater-docker.sh
+++ site-updater-docker.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env sh
 
+## Set additional docker run arguments by changing the variable
+## i2p_www_docker_run_args
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" || exit
+
 git pull origin master
 docker build -t i2p-mirror/i2p.www .
 docker rm -f mirror.i2p.www
-docker run -it --name mirror.i2p.www -p 0.0.0.0:8090:80 i2p-mirror/i2p.www
+docker run -it $i2p_www_docker_run_args --name mirror.i2p.www -p 0.0.0.0:8090:80 i2p-mirror/i2p.www
 #docker run -td --name mirror.i2p.www --restart=always -p 0.0.0.0:5000:5000 i2p-mirror/i2p.www
