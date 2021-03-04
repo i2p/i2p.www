573f33df2bfa6dbd541c822b98381975777747ae
idk <hankhill19580@gmail.com>
2021-03-04 14:58:20 -0500

    cd to $DIR in site-updater-docker.sh

--- site-updater-docker.sh
+++ site-updater-docker.sh
@@ -4,6 +4,7 @@
 ## i2p_www_docker_run_args
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" || exit
+cd "$DIR"
 
 git pull origin master
 docker build -t i2p-mirror/i2p.www .
