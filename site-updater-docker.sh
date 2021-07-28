3206ebf8577b9fa176a521b62d1ebfe22c922883
idk <hankhill19580@gmail.com>
2021-07-28 08:48:22 -0400

    fix broken link to detailed Chromium+extension instructions on browser-config

--- site-updater-docker.sh
+++ site-updater-docker.sh
@@ -9,5 +9,5 @@ cd "$DIR"
 git pull origin master
 docker build -t i2p-mirror/i2p.www .
 docker rm -f mirror.i2p.www
-docker run -it $i2p_www_docker_run_args --name mirror.i2p.www -p 0.0.0.0:8090:80 i2p-mirror/i2p.www
+docker run -itd $i2p_www_docker_run_args --name mirror.i2p.www -p 0.0.0.0:8090:80 i2p-mirror/i2p.www
 #docker run -td --name mirror.i2p.www --restart=always -p 0.0.0.0:5000:5000 i2p-mirror/i2p.www
