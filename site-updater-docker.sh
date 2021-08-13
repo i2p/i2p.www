c968066ae8ba10c34e45fd81c92405a565c9a63b
idk <hankhill19580@gmail.com>
2021-08-13 16:43:44 -0400

    Fixup some language on firefox.html

--- site-updater-docker.sh
+++ site-updater-docker.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" || exit
 cd "$DIR"
 
 git pull origin master
-docker build -t i2p-mirror/i2p.www .
+docker build --no-cache -t i2p-mirror/i2p.www .
 docker rm -f mirror.i2p.www
 docker run -itd $i2p_www_docker_run_args --name mirror.i2p.www -p 0.0.0.0:8090:80 i2p-mirror/i2p.www
 #docker run -td --name mirror.i2p.www --restart=always -p 0.0.0.0:5000:5000 i2p-mirror/i2p.www
