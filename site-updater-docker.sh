640dfed4b4b6db7e1ee2bcf179f9409ab34ca8f6
idk <hankhill19580@gmail.com>
2022-03-24 16:05:45 -0400

    Browser config page updates, add extension names to extension section headings

--- site-updater-docker.sh
+++ site-updater-docker.sh
@@ -50,6 +50,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" || exit
 cd "$DIR"
 
 git pull origin $i2p_www_branch
-docker build $i2p_www_docker_build_args -t i2p-mirror/i2p.www$suffix .
+docker build $i2p_www_docker_build_args -t i2p-mirror/mirror.i2p.www$suffix .
 docker rm -f mirror.i2p.www$suffix
 docker run $i2p_www_docker_run_args --name mirror.i2p.www$suffix -p 0.0.0.0:$port:80 i2p-mirror/i2p.www$suffix $@
